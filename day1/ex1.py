d85dd749d4d15bcfbad4019a49348abbb33ab449
Amalie Mansåker <amalie.mansaker@gmail.com>
2022-12-01 22:35:59 +0100

    Improved splitting data

--- day1/ex1.py
+++ day1/ex1.py
@@ -1,17 +1,13 @@
 with open("input.txt") as file:
-    data = file.read().splitlines()
-    caloriesPerElf = []
-    elf = []
-    for i in range(len(data)):
-        if data[i] != "":
-            elf.append((int(data[i])))
-        else:
-            caloriesPerElf.append(elf)
-            elf = []
-    caloriesPerElf.append(elf)
+    data = file.read().split("\n\n")
+
+for i in range(len(data)):
+    data[i] = data[i].split()
+    for j in range(len(data[i])):
+        data[i][j] = int(data[i][j])
 
 summedCalories = []
-for i in caloriesPerElf:
+for i in data:
     summedCalories.append(sum(i))
 
 
